fe46795ac1b6416111b5333a4f0df6b313b7b80f
queyiwen <queyiwen@bytedance.com>
2020-05-06 10:25:10 +0800

    init

---
+++ src/main/java/com/clickhouse/test/ClickhouseTestApplication.java
@@ -0,0 +1,13 @@
+package com.clickhouse.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickhouseTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickhouseTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clickhouse/test/ClickhouseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clickhouse.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickhouseTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
